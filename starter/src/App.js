fe1978d02f45ce5e1683e717b4f7195924793998
aboublef <kristopherab@gmail.com>
2022-10-20 09:19:46 +0100

    refactored

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,99 +1,12 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { get, getAll, search, update } from "./api/books.js";
-
-const { log: $, table: t } = console;
-
-const useBook = ({ book, __foobar__ }) => {
-    /* NOTE coercing the value of book to `none` if it is undefined.
-    There may be a better away to deal with this but for now this is ok */
-    const [shelf, setShelf] = useState(book.shelf ?? (book.shelf = "none"));
-
-    useEffect(() => {
-        (async () => {
-            /* NOTE I shouldn't need to check if the value is undefined
-            as that is being resolved in the useState function */
-            // $(`before :: book.shelf = ${book.shelf}; shelf = ${shelf}`);
-            if (book.shelf !== shelf) {
-                __foobar__({ book, shelf: (book.shelf = shelf) });
-            }
-            // $(`after :: book.shelf = ${book.shelf}; shelf = ${shelf}`);
-        })();
-    }, [shelf, book, __foobar__]);
-
-    return {
-        shelf,
-        onUpdateShelf: (e) => setShelf(e.target.value ?? "none")
-    };
-};
-
-function Book({ book, __foobar__ }) {
-    const { shelf, onUpdateShelf } = useBook({ book, __foobar__ });
-    return (
-        <div className="book">
-            <div className="book-top">
-                <div
-                    className="book-cover"
-                    style={{ width: 128, height: 188, backgroundImage: `url("${book.imageLinks?.thumbnail}")` }}
-                ></div>
-                <div className="book-shelf-changer">
-                    <select value={shelf} onChange={onUpdateShelf}>
-                        <option value="none" disabled>
-                            Move to...
-                        </option>
-                        <option value="currentlyReading">
-                            Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
-                </div>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
-        </div>
-    );
-}
+import { get, getAll, update } from "./api/books.js";
+import BookPage from "./view/BookPage";
+import SearchPage from "./view/SearchPage";
 
 const useApp = () => {
-    const [route, setRoute] = useState(true);
-
-    const [query, setQuery] = useState("");
-    const [searchedBooks, setSearchedBooks] = useState([]);
-
+    const [route, setRoute] = useState(false);
     const [bookShelves, setBookShelves] = useState({ currentlyReading: [], wantToRead: [], read: [] });
-
-    useEffect(() => {
-        $("app component called");
-    }, []);
-
-
-
-    useEffect(() => {
-        (async () => {
-            if (query === "") {
-                $("reset searched");
-                setSearchedBooks([]);
-                return;
-            };
-
-            const [searched] = await Promise.all([search(query, 20)]);
-            if (searched.error) return setSearchedBooks([]);
-
-            // TODO add results from `getAll`
-            $(Object.values(bookShelves).flat());
-            for (const book of Object.values(bookShelves).flat()) {
-                const found = searched.findIndex(({ id }) => id === book.id);
-                if (found > 0) {
-                    $(found, book);
-                    searched[found].shelf = book.shelf;
-                }
-            };
-
-            setSearchedBooks(searched);
-        })();
-    }, [query, bookShelves]);
 
 
     useEffect(() => {
@@ -112,10 +25,10 @@ const useApp = () => {
     }, []);
 
     /**
-     * Function name `__foobar__` is just a placeholder, subject to change
+     * Function name `onMoveBook` is just a placeholder, subject to change
      * @param {{book: any, shelf:string}} param0 
      */
-    const __foobar__ = async ({ book, shelf }) => {
+    const onMoveBook = async ({ book, shelf }) => {
         const { currentlyReading, wantToRead, read } = await update(book, shelf);
         /* NOTE this is much slower than my old implementation but seems to work anyway */
         const books = await Promise.all([...currentlyReading, ...wantToRead, ...read].map(get));
@@ -133,83 +46,17 @@ const useApp = () => {
     return {
         route,
         onRoute: () => setRoute(!route),
-        query,
-        onQuery: (e) => setQuery(e.target.value ?? "") && (e.target.value = ""),
-        resetQuery: () => setQuery(""), // NOTE this is for a bug that I can't seem to fix
-        searchedBooks,
-        shelvedBooks: bookShelves,
-        __foobar__,
+        bookShelves,
+        onMoveBook,
     };
 };
 
 function App() {
-    const { route, onRoute, onQuery, resetQuery, searchedBooks, shelvedBooks, __foobar__ } = useApp();
+    const { route, onRoute, bookShelves, onMoveBook } = useApp();
 
     return (
         <div className="app">
-            {route ? (
-                <div className="search-books">
-                    <div className="search-books-bar">
-                        <button
-                            className="close-search"
-                            onClick={() => { resetQuery(); onRoute(); }}
-                        >
-                            Close
-                        </button>
-                        <div className="search-books-input-wrapper">
-                            <input
-                                onChange={onQuery}
-                                type="text"
-                                placeholder="Search by title, author, or ISBN"
-                            />
-                        </div>
-                    </div>
-                    <div className="search-books-results">
-                        <ol className="books-grid">
-                            {searchedBooks.map((book => (<li key={book.id}>
-                                <Book {...{ book, __foobar__ }} />
-                            </li>)))}
-                        </ol>
-                    </div>
-                </div>
-            ) : (
-                <div className="list-books">
-                    <div className="list-books-title">
-                        <h1>MyReads</h1>
-                    </div>
-                    <div className="list-books-content">
-                        <div>
-                            <div className="bookshelf">
-                                <h2 className="bookshelf-title">Currently Reading</h2>
-                                <div className="bookshelf-books">
-                                    <ol className="books-grid">{shelvedBooks.currentlyReading?.map((book => (<li key={book.id}>
-                                        <Book {...{ book, __foobar__ }} />
-                                    </li>)))}</ol>
-                                </div>
-                            </div>
-                            <div className="bookshelf">
-                                <h2 className="bookshelf-title">Want to Read</h2>
-                                <div className="bookshelf-books">
-                                    <ol className="books-grid">{shelvedBooks.wantToRead?.map((book => (<li key={book.id}>
-                                        <Book {...{ book, __foobar__ }} />
-                                    </li>)))}</ol>
-                                </div>
-                            </div>
-                            <div className="bookshelf">
-                                <h2 className="bookshelf-title">Read</h2>
-                                <div className="bookshelf-books">
-                                    <ol className="books-grid">{shelvedBooks.read?.map((book => (<li key={book.id}>
-                                        <Book {...{ book, __foobar__ }} />
-                                    </li>)))}</ol>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="open-search">
-                        <button onClick={onRoute}>Add a book</button>
-                    </div>
-                </div>
-            )}
+            {route ? <SearchPage  {...{ onRoute, bookShelves, onMoveBook }} /> : <BookPage  {...{ onRoute, bookShelves, onMoveBook }} />}
         </div>
     );
 }
